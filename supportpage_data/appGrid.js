fdd1585039b0294025d26238c6a7a08c424e960d
Thomas Sankara <thomassankara74@gmail.com>
2021-01-10 09:43:19 +0300

    support page images and data

---
+++ supportpage_data/appGrid.js
@@ -0,0 +1,43 @@
+const data = [
+  {
+    id: 1,
+    image: './images/images_supportpage/microsoft365.svg',
+    desc: 'Microsoft 365',
+  },
+  {
+    id: 2,
+    image: './images/images_supportpage/office.png',
+    desc: 'Office',
+  },
+  {
+    id: 3,
+    image: './images/images_supportpage/outlook.png',
+    desc: 'Outlook',
+  },
+  {
+    id: 4,
+    image: './images/images_supportpage/onedrive.png',
+    desc: 'OneDrive',
+  },
+  {
+    id: 5,
+    image: './images/images_supportpage/edge.png',
+  },
+  {
+    id: 6,
+    image: './images/images_supportpage/teams.png',
+    desc: 'Microsoft Teams',
+  },
+  {
+    id: 7,
+    image: './images/images_supportpage/xbox.png',
+    desc: 'Xbox',
+  },
+  {
+    id: 8,
+    image: './images/images_supportpage/account.png',
+    desc: 'Account & billing',
+  },
+]
+
+export default data
---
+++ supportpage_data/learningGrid.js
@@ -0,0 +1,68 @@
+const data = [
+  {
+    id: 1,
+    image: './images/images_supportpage/ms365_training.jpg',
+    title: 'Microsoft 365 Training Center',
+    desc:
+      'Get productive quickly with these Microsoft 365 videos, tutorials and resources.',
+    link: 'Browse training options',
+  },
+  {
+    id: 2,
+    image: './images/images_supportpage/remote_teaching.jpg',
+    title: 'Remote teaching and learning',
+    desc:
+      "Access the tools you need for classroom management and student engagement whether you're in the classroom or connecting remotely.",
+    link: 'Get tools for teachers',
+  },
+  {
+    id: 3,
+    image: './images/images_supportpage/ms_tips.jpg',
+    title: 'Microsoft tips',
+    desc:
+      'Explore the latest tips to get the most from your Microsoft products.',
+    link: 'Get tips for windos, surface & more',
+  },
+  {
+    id: 4,
+    image: './images/images_supportpage/work_remotely.jpg',
+    title: 'Work remotely with Microsoft 365',
+    desc:
+      'Collaborate with others seamlessly and get more done with Microsoft Teams and Microsoft 365.',
+    link: 'Browse tips, videos, & more',
+  },
+  {
+    id: 5,
+    image: './images/images_supportpage/new_365.jpg',
+    title: "What's new in Microsoft 365",
+    desc:
+      "As a Microsoft 365 subscriber, you always get the latest features. Discover what's new and improved today.",
+    link: "See what's new in Microsoft 365",
+  },
+  {
+    id: 6,
+    image: './images/images_supportpage/new_windows_10.jpg',
+    title: "What's new in Windows 10",
+    desc:
+      'Windows updates will help you manage your time, increase security, and get more creative with Windows 10.',
+    link: "See what's new in Windows 10",
+  },
+  {
+    id: 7,
+    image: './images/images_supportpage/accessibility.jpg',
+    title: 'Accessibility',
+    desc:
+      'Learn more about accessibility featuress and personalize your PC settings.',
+    link: 'Learn about accessibility features',
+  },
+  {
+    id: 8,
+    image: './images/images_supportpage/family_activites.jpg',
+    title: 'Family activities templates',
+    desc:
+      'Connect with your children, family, or friends with a collection of free activities that everyone can enjoy.',
+    link: 'Choose a template',
+  },
+]
+
+export default data
